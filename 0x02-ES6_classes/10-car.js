546456c5b4311a2618d25acd8548fdd7f0130810
rhf007 <rahofahazem512@gmail.com>
2024-12-05 10:52:43 +0000

    file 10

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,53 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Brand must be a string');
+    }
+
+    this._brand = value;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Motor must be a string');
+    }
+
+    this._motor = value;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Color must be a string');
+    }
+
+    this._color = value;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+
+    return new Species();
+  }
+}
