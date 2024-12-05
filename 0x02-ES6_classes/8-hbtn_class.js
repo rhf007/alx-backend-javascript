242716d2f65e955386b02d38509648e38f42e84a
rhf007 <rahofahazem512@gmail.com>
2024-12-05 10:17:04 +0000

    file 8

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,40 @@
+export default class ALXClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    if (typeof value !== 'number') {
+      throw new Error('Size must be a number');
+    }
+
+    this._size = value;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Location must be a string');
+    }
+
+    this._location = value;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this.size;
+    }
+    if (hint === 'string') {
+      return this.location;
+    }
+    return this;
+  }
+}
