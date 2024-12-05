71c8de7de8106ab4b1194f2a1ff6501481013a41
rhf007 <rahofahazem512@gmail.com>
2024-12-05 10:28:09 +0000

    file 9

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -13,6 +13,9 @@ export class ALXClass {
   }
 }
 
+const class2019 = new ALXClass(2019, 'San Francisco');
+const class2020 = new ALXClass(2020, 'San Francisco');
+
 export class StudentALX {
   constructor(firstName, lastName, alxClass) {
     this._firstName = firstName;
@@ -33,9 +36,6 @@ export class StudentALX {
   }
 }
 
-const class2019 = new ALXClass(2019, 'San Francisco');
-const class2020 = new ALXClass(2020, 'San Francisco');
-
 const student1 = new StudentALX('Guillaume', 'Salva', class2020);
 const student2 = new StudentALX('John', 'Doe', class2020);
 const student3 = new StudentALX('Albert', 'Clinton', class2019);
@@ -43,3 +43,5 @@ const student4 = new StudentALX('Donald', 'Bush', class2019);
 const student5 = new StudentALX('Jason', 'Sandler', class2019);
 
 export const listOfStudents = [student1, student2, student3, student4, student5];
+
+export default listOfStudents;
