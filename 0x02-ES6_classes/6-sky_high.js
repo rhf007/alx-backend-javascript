825905b2d1c9d5720a1bc1189cf48f9d43fa9c89
rhf007 <rahofahazem512@gmail.com>
2024-12-05 09:12:07 +0000

    file 6

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,36 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new Error('SQFT must be a number');
+    }
+
+    this._sqft = value;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    if (typeof floors !== 'number') {
+      throw new Error('Floors must be a number');
+    }
+
+    this._floors = value;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
