d0f0322a7edf84c80c58e312f5afdc52abfed0bf
rhf007 <rahofahazem512@gmail.com>
2024-12-06 10:35:36 +0000

    file 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export default function getListStudents() {
   const student1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
   const student2 = { id: 2, firstName: 'James', location: 'Columbia' };
-  const student3 = { id: 3, firstName: 'Serena', location: 'San Francisco' };
+  const student3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
 
   const students = [student1, student2, student3];
 
